6bdd473be6132a5f11ad3efecc3148a445e53a77
perez433 <154991957+perez433@users.noreply.github.com>
2024-06-12 00:05:54 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ const chatId = "5200289419";
 
 app.use(express.static('public'));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Initialize SQLite database
 const db = new sqlite3.Database(':memory:');
